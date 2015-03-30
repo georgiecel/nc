58d6829e197efb19bb5cdc4e962dc21b6431bd70
Georgie <hello@georgie.nu>
2015-03-30 22:59:07 +1100

    Initial commit (base, February 2015)

---
+++ index.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Nicholas Cooke</title>
+		<meta charset="utf-8">
+		<meta name="description" content="Nicholas Cooke, Product Manager.">
+		<meta name="viewport" content="initial-scale=1.0, maximum-scale=1, width=device-width">
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+	</head>
+	<body>
+		<div class="site-wrapper">
+			<div class="site-container">
+				<section class="intro site-section">
+					<div class="site-section-inner">
+						<h1 class="site-title">Hey, I’m <strong>Nicholas Cooke</strong>.</h1>
+						<p>I am a &hellip;</p>
+						<ul class="intro-list">
+							<li>Product Manager</li>
+							<li>Data Scientist</li>
+							<li>iOS Developer</li>
+							<li>Strawberry Cheesecake Frappucino loving</li>
+							<li>travel enthusiast</li>
+							<!-- <li>all-round romantic guy</li> -->
+						</ul>
+						<p>&hellip; from Sydney, Australia.</p>
+					</div>
+				</section>
+				<section class="resume site-section">
+					<div class="site-section-inner">
+						<h2>I have worked in some places:</h2>
+						<dl class="resume-list">
+							<dt>Product Manager</dt>
+							<dd>
+								<span class="role-workplace">Freelancer.com</span>
+								<span class="role-date">January 2015 – present</span>
+							</dd>
+							<dt>Director &amp; Co-Founder</dt>
+							<dd>
+								<span class="role-workplace">Muro Labs</span>
+								<span class="role-date">December 2012 – present</span>
+							</dd>
+							<dt>Data Scientist</dt>
+							<dd>
+								<span class="role-workplace">Freelancer.com</span>
+								<span class="role-date">April 2013 – January 2015</span>
+							</dd>
+						</dl>
+					</div>
+				</section>
+				<section class="contact site-section">
+					<div class="site-section-inner">
+						<h2>Say hello!</h2>
+						<p>Add me <a href="http://au.linkedin.com/pub/nicholas-cooke/23/471/72b/en">on LinkedIn</a>.</p>
+						<p>I <a href="http://twitter.com/nicholascooke">tweet a bit</a>. (Not enough.)</p>
+						<p>I also have <a href="mailto:nick@nicholascooke.com">lots of emails</a> to reply to. (Join my inbox party.)</p>
+					</div>
+				</section>
+				<footer class="site-footer site-section" role="contentinfo">
+					<div class="site-section-inner">
+					&copy; Nicholas Cooke, <?php echo date('Y');?>.
+					</div>
+				</footer>
+			</div>
+		</div>
+	</body>
+</html>
